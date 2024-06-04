3938f606027c7803edfe823d912364ab354a3bd2
AgengRamadhan <agengramadhan2021@gmail.com>
2024-06-04 21:31:28 +0700

    Project Fragment

---
+++ app/src/main/java/com/example/fragmentgen/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,24 @@
+package com.example.fragmentgen.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.fragmentgen.R;
+import com.example.fragmentgen.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard, null);
+    }
+}
---
+++ app/src/main/java/com/example/fragmentgen/ui/profile/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.example.fragmentgen.ui.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fragmentgen.R;
+
+public class ProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+}
